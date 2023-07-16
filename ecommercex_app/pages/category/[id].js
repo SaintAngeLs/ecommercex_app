0f09e896cc8359d4f72e69f7efa88d201ee53a68
A.Voznesenskyi <voznesenskijandrej5@gmail.com>
2023-07-16 23:18:54 +0200

    api filtering

--- ecommercex_app/pages/category/[id].js
+++ ecommercex_app/pages/category/[id].js
@@ -1,9 +1,11 @@
 import Center from "@/components/Center";
 import Header from "@/components/Header";
 import ProductsGrid from "@/components/ProductsGrid";
+import Spinner from "@/components/Spinner";
 import { Category } from "@/models/Category";
 import { Product } from "@/models/Product";
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 
@@ -37,14 +39,49 @@ const Filter = styled.div`
 `;
 
 
-export default function CategoryPage({category, subCategories, products}){
-
+export default function CategoryPage({category, subCategories, products:originalProducts}){
     const defaultSortingValue = '_id-desc';
+    const defaultFilterValues = category.properties.map(p => ({name:p.name, value:'all'}));
     const [sort,setSort] = useState(defaultSortingValue);
     const [filtersChanged,setFiltersChanged] = useState(false);
     const [filtersValues,setFiltersValues] = useState(defaultFilterValues);
     const [loadingProducts,setLoadingProducts] = useState(false);
+    const [products,setProducts] = useState(originalProducts);
 
+    function handleFilterChange(filterName, filterValue){
+        setFiltersValues(previous => {
+            return previous.map(prev => ({
+                name: prev.name,
+                value: prev.name === filterName ? filterValue : prev.value,
+            }));
+        });
+        setFiltersChanged(true);
+    }
+
+    useEffect(() => {
+        if(!filtersChanged)
+        {
+            return;
+        }
+
+        setLoadingProducts(true);
+
+        const categoryIds = [category._id, ...(subCategories?.map(category => category._id) || [])];
+        const params = new URLSearchParams;
+        params.set('categories', categoryIds.join(','));
+        params.set('sort', sort);
+        filtersValues.forEach(filterF => {
+        if (filterF.value !== 'all') 
+        {
+            params.set(filterF.name, filterF.value);
+        }
+        });
+        const url = `/api/products?` + params.toString();
+        axios.get(url).then(res => {
+        setProducts(res.data);
+        setLoadingProducts(false);
+        })
+    }, [filtersValues, sort, filtersChanged]);
 
     return (
         <>
