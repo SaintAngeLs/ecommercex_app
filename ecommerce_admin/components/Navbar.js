0073c8737bf361be090e6b91f7869570ba0c9cb1
A.Voznesenskyi <voznesenskijandrej5@gmail.com>
2023-09-19 20:58:50 +0200

    udpating the styles of the Navbar.js and the HomeHeader component

--- ecommerce_admin/components/Navbar.js
+++ ecommerce_admin/components/Navbar.js
@@ -5,7 +5,7 @@ import React from 'react'
 import Logo from './Logo';
 
 const Navbar = ({show}) => {
-  const inActiveLink = "flex gap-2 p-1";
+  const inActiveLink = "flex gap-2 p-1 hover:bg-gray-200 hover:text-black";
   const activeLink = "bg-white text-black rounded-tl-lg rounded-bl-lg " + inActiveLink;
   const inactiveIcon = 'h-6 w-6';
   const activeIcon = inactiveIcon + ' text-primary';
@@ -23,7 +23,7 @@ const Navbar = ({show}) => {
         </div>
         
         <nav className='flex flex-col gap-3'>
-            <Link href ={'/'} className = {pathname === "/" ? activeLink : inActiveLink }>
+            <Link href ={'/'} className = {pathname === "/" ? activeLink : inActiveLink } >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className = {pathname === "/" ? activeIcon : inactiveIcon }>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
                 </svg>
